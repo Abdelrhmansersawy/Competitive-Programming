88cb28f057311f013fd28831754c1a20b607ea8a
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2023-04-08 12:14:25 +0200

    Update nCr_DP.cpp

--- Algrothims/nCr_DP.cpp
+++ Algrothims/nCr_DP.cpp
@@ -10,7 +10,7 @@ int main(){
     for (int i = 1; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if(j == 0) nCr[i][j] = 1;
-            nCr[i][j] = (nCr[i-1][j] + nCr[i-1][j-1])%mod;
+            nCr[i][j] = (nCr[i-1][j] + nCr[i-1][j-1]);
         }
     }
 }

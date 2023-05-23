c99e3d7ab79074c5ce6fb7800ec3a4f4d4e45bc7
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2023-05-23 23:27:52 +0300

    Update mobius.cpp

--- Algrothims/mobius.cpp
+++ Algrothims/mobius.cpp
@@ -8,7 +8,7 @@ int mobius[N] , sieve[N];
 void gen_mobius(){
   for(int i = 1; i < N; i++) {mobius[i] = sieve[i] = 1;}
   sieve[1] = 0;
-  for(int i = 2; i * i < N; i++){
+  for(int i = 2; i < N; i++){
     if(sieve[i]){
       for(int j = i; j < N; j += i){
         sieve[j] = 0;

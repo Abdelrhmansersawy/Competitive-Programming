b56fe2a09705013ca48691a187cc0d69ac300141
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2023-08-05 21:23:33 +0300

    fix problem on sparse_table.cpp

--- Algrothims/sparse_table.cpp
+++ Algrothims/sparse_table.cpp
@@ -2,7 +2,7 @@ const int N = 1e5 , M = 18;
 int mn[N][M], mx[N][M] , a[N] , n;
 void gen_sparsetable(){
 	for (int i = 0; i < n; i++) mn[i][0] = mx[i][0] = a[i];
-    for (int j = 1; (1 << j) < n; j++){
+    for (int j = 1; (1 << j) <= n; j++){
         for (int i = 0; (i + (1 << j) - 1) < n; i++){
        		mn[i][j] = min(mn[i][j - 1] , mn[i + (1 << (j - 1))][j - 1]);
     	  	mx[i][j] = max(mx[i][j - 1] , mx[i + (1 << (j - 1))][j - 1]);

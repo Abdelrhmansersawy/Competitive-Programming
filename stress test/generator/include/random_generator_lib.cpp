a35e030e1dc6eb1e2d8421aaea08964f9517db27
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2023-08-10 02:04:54 +0300

    Update random_generator_lib.cpp

--- stress test/generator/include/random_generator_lib.cpp
+++ stress test/generator/include/random_generator_lib.cpp
@@ -54,13 +54,13 @@ std::string random_string(int length)
 {
     std::string str(length, ' ');
     for (int i = 0; i < length; ++i)
-        str[i] = 'a' + random32(0, 26);
+        str[i] = 'a' + random32(0, 25);
 
     return str;
 }
 
 /**
- * @brief pick random item fron vector of items
+ * @brief pick random item from vector of items
  *
  * @tparam T the data type of vector
  * @param vec the container
@@ -77,7 +77,7 @@ T pick_random(std::vector<T> &vec)
 }
 
 /**
- * @brief pick random item fron vector of items then remove it from the vector
+ * @brief pick random item from vector of items then remove it from the vector
  *
  * @tparam T the data type of vector
  * @param vec the container
@@ -130,43 +130,147 @@ std::vector<std::pair<int, int>> random_tree(int number_of_nodes = -1, int root 
         height = number_of_nodes - 1;
 
     std::vector<std::pair<int, int>> edges;
-    std::vector<int> nodes_of_depth[height + 1];
-    std::vector<int> nodes(number_of_nodes);
-    iota(nodes.begin(), nodes.end(), 1);
-    std::shuffle(nodes.begin(), nodes.end(), random_seed);
+    int depth[number_of_nodes];
+    std::vector<int> nodes_to_connect, disconnected_nodes(number_of_nodes);
+    iota(disconnected_nodes.begin(), disconnected_nodes.end(), 1);
 
-    nodes_of_depth[0] = {root};
+    depth[root - 1] = 0;
+    nodes_to_connect.emplace_back(root);
     for (int h = 1; h <= height; ++h)
     {
-        int random_node = pick_random_and_remove(nodes);
-        if (random_node == root)
+        int random_disconnected_node = pick_random_and_remove(disconnected_nodes);
+        if (random_disconnected_node == root)
         {
             --h;
             continue;
         }
 
         if (edges.empty())
-            edges.push_back({root, random_node});
+        {
+            depth[random_disconnected_node - 1] = 1 + depth[root - 1];
+            edges.push_back({root, random_disconnected_node});
+        }
         else
-            edges.push_back({edges.back().second, random_node});
+        {
+            depth[random_disconnected_node - 1] = 1 + depth[edges.back().second - 1];
+            edges.push_back({edges.back().second, random_disconnected_node});
+        }
 
-        nodes_of_depth[h].emplace_back(random_node);
+        if (depth[random_disconnected_node - 1] != height)
+            nodes_to_connect.emplace_back(random_disconnected_node);
     }
 
-    while (!nodes.empty())
+    while (!disconnected_nodes.empty())
     {
-        int random_not_connected_node = pick_random_and_remove(nodes);
-        if (random_not_connected_node == root)
+        int random_disconnected_node = pick_random_and_remove(disconnected_nodes);
+        if (random_disconnected_node == root)
             continue;
+        int random_connected_node = pick_random(nodes_to_connect);
 
-        int random_depth = random32(0, height - 1);
-        int random_connected_node = pick_random(nodes_of_depth[random_depth]);
+        depth[random_disconnected_node - 1] = 1 + depth[random_connected_node - 1];
+        edges.push_back({random_connected_node, random_disconnected_node});
 
-        edges.push_back({random_connected_node, random_not_connected_node});
-        nodes_of_depth[random_depth + 1].push_back(random_not_connected_node);
+        if (depth[random_disconnected_node - 1] != height)
+            nodes_to_connect.emplace_back(random_disconnected_node);
     }
 
     std::shuffle(edges.begin(), edges.end(), random_seed);
 
     return edges;
+}
+
+/**
+ * @brief generates a random permutation in O(length)
+ *
+ * @param length
+ * @return std::vector<int>
+ */
+std::vector<int> random_permutation(int length)
+{
+    if (length < 0)
+        throw std::invalid_argument("can not generate negative size for permutation");
+
+    std::vector<int> vec(length);
+    std::iota(vec.begin(), vec.end(), 1);
+
+    std::vector<int> randomized_vec;
+    while (length--)
+        randomized_vec.push_back(pick_random_and_remove(vec));
+
+    return randomized_vec;
+}
+
+/**
+ * @brief generates a random binary string in O(length)
+ *
+ * @param length
+ * @return std::string
+ */
+std::string random_binary_string(int length)
+{
+    if (length < 0)
+        throw std::invalid_argument("can not generate negative size for string");
+
+    std::string str;
+    while (length--)
+        str.push_back('0' + random32(0, 1));
+
+    return str;
+}
+
+/**
+ * @brief return a random Boolean Value True/False in O(1)
+ *
+ * @return bool
+ */
+
+bool random_flag()
+{
+    return random32(0, 1);
+}
+
+/**
+ * @brief returns a random vowel character
+ *
+ * @param isUpper optoinal(bool). In case not given or pass `false` function will return lower vowel character
+ * otherwise function will return upper vowel character
+ * @return char
+ */
+
+char random_vowel(bool isUpper=false)
+{
+    std::vector<char> vowels{'a', 'e', 'i', 'o', 'u'};
+    char chosen = pick_random(vowels);
+    return isUpper ? toupper(chosen) : chosen;
+}
+
+/**
+ * @brief returns a matrix with 'row' rows and 'col' columns and the values in the matrix are in range [low, high]
+ *
+ * @param row - number of rows in the matrix
+ * @param col - number of columns in the matrix
+ * @param low - starting range
+ * @param high - ending range
+ * @return std::vector<std::vector<long long>>
+ */
+
+std::vector<std::vector<long long>> random_matrix(int row, int col, long long low, long long high)
+{
+    if (low > high)
+    {
+        throw std::invalid_argument("range is invalid");
+    }
+    if (row == 0 || col == 0)
+    {
+        throw std::invalid_argument("row or column can't be empty");
+    }
+    std::vector<std::vector<long long>> matrix(row, std::vector<long long>(col, 0));
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < col; ++j)
+        {
+            matrix[i][j] = random64(low, high);
+        }
+    }
+    return matrix;
 }

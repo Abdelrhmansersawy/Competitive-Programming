17c250c00b350863f1865a22d5bb345aaa56a4b7
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2023-09-20 15:44:40 +0300

    Update nCr_DP.cpp

--- Algrothims/Math/nCr_DP.cpp
+++ Algrothims/Math/nCr_DP.cpp
@@ -1,15 +1,10 @@
-#include <bits/stdc++.h>
-typedef long long ll;
-#define s second
-#define f first
-using namespace std;
-int main(){
-  ll n; cin >> n;
-  ll nCr[n][n];
-  nCr[0][0] = 1;
-    for (int i = 1; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if(j == 0) nCr[i][j] = 1;
+const int N = 61;
+ll nCr[N][N];
+void gen(){
+    nCr[0][0] = 1;
+    for (int i = 0; i < N; ++i) {
+        nCr[i][0] = nCr[i][i] = 1;
+        for (int j = 0; j < i; ++j) {
             nCr[i][j] = (nCr[i-1][j] + nCr[i-1][j-1]);
         }
     }

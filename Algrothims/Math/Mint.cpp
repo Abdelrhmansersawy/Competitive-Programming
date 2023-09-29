c447fc19065338e97317ceede5ad05f199ceb564
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2023-09-29 19:06:25 +0300

    Update Mint.cpp

--- Algrothims/Math/Mint.cpp
+++ Algrothims/Math/Mint.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 typedef long long ll;
 #define rep(i , st , ed) for(int i = st; i < ed; i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+
 #define f first
 #define s second
 template<int MOD> struct ModInt {
@@ -27,17 +29,16 @@ template<int MOD> struct ModInt {
 template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
 template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
     ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
-template<typename T, int FAC_MAX> struct Comb { 
-    vector<T> fac, ifac;
+template<typename T, int FAC_MAX> struct Comb { vector<T> fac, ifac;
     Comb(){fac.resize(FAC_MAX,1);ifac.resize(FAC_MAX,1);rep(i,1,FAC_MAX)fac[i]=fac[i-1]*i;
-        ifac[FAC_MAX-1]=T(1)/fac[FAC_MAX-1];rep(i,FAC_MAX-2,1)ifac[i]=ifac[i+1]*T(i+1);}
+        ifac[FAC_MAX-1]=T(1)/fac[FAC_MAX-1];rrep(i,FAC_MAX-2,1)ifac[i]=ifac[i+1]*T(i+1);}
     T aPb(int a, int b) { if (b < 0 || a < b) return T(0); return fac[a] * ifac[a - b]; }
     T aCb(int a, int b) { if (b < 0 || a < b) return T(0); return fac[a] * ifac[a - b] * ifac[b]; }
     T nHk(int n, int k) { if (n == 0 && k == 0) return T(1); if (n <= 0 || k < 0) return 0;
         return aCb(n + k - 1, k); } // nHk = (n+k-1)Ck : n is separator
     T pairCombination(int n) {if(n%2==1)return T(0);return fac[n]*ifac[n/2]/(T(2)^(n/2));}
     // combination of paris for n
-};
+}; 
 typedef ModInt<998244353> mint;
 int main(){
     ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(0);
@@ -46,6 +47,6 @@ int main(){
     freopen("out.txt", "w", stdout);
     freopen("error.txt", "w", stderr);
     #endif 
-    Comb<mint , (int) 1e6 + 1> com;
-    cout << com.aCb(3 , 2);
+    const int N = 1e5;
+    Comb<mint , N> com;
 }

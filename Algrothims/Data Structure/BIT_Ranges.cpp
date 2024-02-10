92618f1b74904da5307a65b359e9e3cfd367fe2e
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-02-10 14:29:50 +0200

    Create BIT_Ranges.cpp

---
+++ Algrothims/Data Structure/BIT_Ranges.cpp
@@ -0,0 +1,32 @@
+const int N = 2e5 + 9;
+class BITrange {
+private:
+    long long m[N], c[N];
+    void add(int pos, long long mVal, long long cVal) {
+        for (++pos; pos <= N; pos += (pos & (-pos))) {
+            m[pos - 1] += mVal;
+            c[pos - 1] += cVal;
+        }
+    }
+    long long get(int pos) {
+        long long ret = 0;
+        int x = pos;
+        for (++pos; pos; pos -= (pos & (-pos))) {
+            ret += m[pos - 1] * x + c[pos - 1];
+        }
+        return ret;
+    }
+    
+public:
+    void init(int n) {
+        memset(m, 0, n * sizeof(m[0]));
+        memset(c, 0, n * sizeof(m[0]));
+    }
+    void addRange(int st, int en, long long val) {
+        if(st > en) return;
+        add(st, val, -val * (st - 1));
+        add(en + 1, -val, val * en);
+    }
+    void addIndex(int i, ll val){ addRange(i,i,val); }
+    ll getIndex(int i){ return get(i) - get(i - 1); }
+};

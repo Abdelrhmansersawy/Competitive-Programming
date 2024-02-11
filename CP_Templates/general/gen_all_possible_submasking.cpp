44ff1f23375ca3286171e22bd862f08152b31101
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-02-11 14:47:20 +0200

    Delete CP_Templates/general directory

--- CP_Templates/general/gen_all_possible_submasking.cpp
+++
@@ -1,7 +0,0 @@
-/*
-	----> Generating All possible submasks <-----
-	- Iterating through all masks with their submasks. Complexity  O(3^n)
-*/
-// m : mask , s : submask
-for (int m=0; m<(1<<n); ++m)
-    for (int s=m; s; s=(s-1)&m)

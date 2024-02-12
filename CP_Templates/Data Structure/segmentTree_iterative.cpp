31e0691080b7d86f06d7be372782e4c95fdf2044
abdelrhmansersawy <abdelrhmansersawy@gmail.com>
2024-02-13 01:39:15 +0200

    implement iterative segment tree, HLD algrothims

---
+++ CP_Templates/Data Structure/segmentTree_iterative.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i , st , ed) for(int i = st; i < ed; i++)
+#define f first
+#define s second
+#define all(v) v.begin() , v.end()
+#ifndef ONLINE_JUDGE 
+#define debug(x) cerr << #x << ": " << x << '\n';
+#else
+#define debug(x)
+#endif
+const int N = 2e5 + 9;
+int seg[4 * N];
+void update(int k, int x) {
+    k += N; seg[k] = x; k >>= 1;
+    while (k > 0) {
+        seg[k] = max(seg[2*k], seg[2*k+1]);
+        k >>= 1;
+    }
+}
+int query(int a, int b) {
+    a += N, b += N;
+    int s = 0;
+    while (a <= b) {
+        if (a & 1) {
+            s = max(s, seg[a]);
+            a++;
+        }
+        if (~b & 1) {
+            s = max(s, seg[b]);
+            b--;
+        }
+        a >>= 1, b >>= 1;
+    }
+    return s;
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif
+
+}

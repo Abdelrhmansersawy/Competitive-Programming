e5627a1f438a0df510cd29111d00e46554e6a463
abdelrhmansersawy <abdelrhmansersawy@gmail.com>
2024-03-03 03:07:17 +0200

    add stress test

--- stress test/using_diff/script.sh
+++ stress test/using_diff/script.sh
@@ -1,17 +1,9 @@
 set -e
-g++ code.cpp -o code
-g++ gen.cpp -o gen
-g++ brute.cpp -o brute
-for((i = 1; ; ++i)); do
-    ./gen $i > input_file
-    ./code < input_file > myAnswer
-    ./brute < input_file > correctAnswer
-    diff -Z myAnswer correctAnswer > /dev/null || break
+
+for((i = 1; i < 10; ++i)); do
+
     echo "Passed test: "  $i
 done
-echo "WA on the following test:"
-cat input_file
-echo "Your answer is:"
-cat myAnswer
-echo "Correct answer is:"
-cat correctAnswer
+
+cat input_file  > failtest.txt
+open failtest.txt

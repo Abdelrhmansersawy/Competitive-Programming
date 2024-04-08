b35cbe04c3609a5935706dc124b2f156a5b03b5e
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-04-09 00:06:30 +0200

    Update gcc_optimize.cpp

--- CP_Templates/Snippet/gcc_optimize.cpp
+++ CP_Templates/Snippet/gcc_optimize.cpp
@@ -5,3 +5,11 @@
 #pragma GCC target("avx2")
 // runtime errors with overflow
 #pragma GCC optimize("trapv")
+
+
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+ 

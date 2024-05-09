9c77455e0c7502d8532164571b3288c4c4c51a03
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-05-09 16:14:08 +0300

    Update Matrix_Exponential.cpp

--- CP_Templates/Math/Counting/Matrix_Exponential.cpp
+++ CP_Templates/Math/Counting/Matrix_Exponential.cpp
@@ -1,31 +1,34 @@
-int n;
-struct Matrix {
-    vector<vector<ll>> a = vector<vector<ll>> (n , vector<ll>(n , 0));
-    Matrix operator *(Matrix& other){
-        Matrix prod;
-        for (int i = 0; i < n; ++i)
-        {
-            for (int j = 0; j < n; ++j)
-            {
-                for (int k = 0; k < n; ++k)
-                {
-                    prod.a[i][k] = ( prod.a[i][k] + a[i][j] * other.a[j][k] ) % mod;
+template<class T = int>
+struct Mat{
+    vector<vector<T>>a;
+    int r , c;
+    Mat(int r , int c){
+        this->r = r; this->c = c;
+        a.resize(c , vector<T>(c, 0));
+    };
+    Mat operator *(const Mat& b){
+        Mat prod(r , b.c);
+        assert(c==b.r);
+        for(int i=0;i<r;i++){
+            for(int k=0;k<c;k++){
+                if( a[i][k] != 0 ){ // Just Optimized 
+                    for(int j=0;j<b.c;j++){
+                        prod.a[i][j] += a[i][k]*b.a[k][j] % mod;
+                        prod.a[i][j]=(prod.a[i][j]+mod) % mod;
+                    }
                 }
             }
         }
         return prod;
     }
 };
-Matrix matrix_exp(Matrix a , ll k){
-    Matrix prod;
-    for (int i = 0; i < n; ++i)
-    {
-        prod.a[i][i] = 1;
+Mat bpw(Mat a , ll b){
+    Mat res(a.r , a.c);
+    for(int i = 0; i < res.r ; i++) res.a[i][i] = 1;
+    while( b > 0){
+        if(b & 1) res = res * a;
+        a = a*a;
+        b >>=1;
     }
-    while(k){
-        if(k % 2) prod = prod * a;
-        k /= 2;
-        a = a * a;
-    }
-    return prod;
+    return res;
 }

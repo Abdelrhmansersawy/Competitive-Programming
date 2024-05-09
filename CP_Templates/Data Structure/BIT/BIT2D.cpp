52221a4ce3509fe837a3ceb76d29030841b7366e
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-05-09 16:24:43 +0300

    Update BIT2D.cpp

--- CP_Templates/Data Structure/BIT/BIT2D.cpp
+++ CP_Templates/Data Structure/BIT/BIT2D.cpp
@@ -1,19 +1,20 @@
 /*
-	--------------BIT2D-----------------
-	Use: 1 - add value to element in grid(
-		 2 - sum of rectangle in grid 
-	Base: 0-index
-	Time complexity : add , sum >> O(log(n) * log(m))
+    --------------BIT2D-----------------
+    Use: 1 - add value to element in grid(
+         2 - sum of rectangle in grid 
+    Base: 0-index
+    Time complexity : add , sum >> O(log(n) * log(m))
 
 */
+template<class T = int>
 struct BIT2D {
-    vector<vector<int>> tree;
+    vector<vector<T>> tree;
     int n , m;
     void init(int n , int m) {
         this->n = n; this->m = m;
-        tree.assign(n , vector<int>(m , 0));
+        tree.assign(n , vector<T>(m , 0));
     }
-    void add(int x, int y, int val) {
+    void add(int x, int y, T val) {
         for (int i = x + 1; i <= n; i += (i & (-i))) {
             for (int j = y + 1; j <= m; j += (j & (-j))) {
                 tree[i - 1][j - 1] += val;
@@ -21,8 +22,8 @@ struct BIT2D {
         }
     }
 
-    int sum(int x, int y) {
-        int ret = 0;
+    T sum(int x, int y) {
+        T ret = 0;
         for (int i = x + 1; i; i -= (i & (-i))) {
             for (int j = y + 1; j; j -= (j & (-j))) {
                 ret += tree[i - 1][j - 1];
@@ -31,8 +32,8 @@ struct BIT2D {
         return ret;
     }
 
-    int sum(int sx, int sy, int ex, int ey) {
+    T qru(int sx, int sy, int ex, int ey) {
         return sum(ex, ey) - sum(ex, sy - 1) - sum(sx - 1, ey) + sum(sx - 1, sy - 1);
     }
-    int getval(int x , int y){ return sum(x , y , x , y); }
+    T qru(int x , int y){ return sum(x , y , x , y); }
 };

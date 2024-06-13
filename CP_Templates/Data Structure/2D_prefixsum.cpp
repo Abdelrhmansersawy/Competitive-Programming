41a06f43eccb68e5494dba4a376110928710b61b
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-06-13 22:17:52 +0300

    Update 2D_prefixsum.cpp

--- CP_Templates/Data Structure/2D_prefixsum.cpp
+++ CP_Templates/Data Structure/2D_prefixsum.cpp
@@ -1,38 +1,22 @@
-/*
-Problem Link: https://cses.fi/problemset/task/1652
-*/
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define rep(i , st , ed) for(int i = st; i < ed; i++)
-#define f first
-#define s second
-#define all(v) v.begin() , v.end()
-#ifndef ONLINE_JUDGE 
-#define debug(x) cerr << #x << ": " << x << '\n';
-#else
-#define debug(x)
-#endif
-int main(){
-    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(0);
-    #ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    freopen("error.txt", "w", stderr);
-    #endif
-    int n , q; cin >> n >> q;
-    int pre[n + 1][n + 1];
-    memset(pre , 0 ,sizeof pre);
-    rep(i ,  1,  n + 1) rep(j , 1 , n + 1){
-        char x; cin >> x;
-        if(x == '*') pre[i][j]++;
-        pre[i][j] += pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
+const int N = 500, M = 500;
+int pref[N][M]; // 1-base
+int n , m;
+int qry(int from_x , int from_y , int to_x , int to_y){
+    return  pref[to_x][to_y] - pref[from_x - 1][to_y] - pref[to_x][from_y - 1] + pref[from_x - 1][from_y - 1];
+}
+void upd(int from_x , int from_y, int to_x , int to_y, int val){
+    pref[from_x][from_y] += val;
+    pref[to_x + 1][from_y] -= val;
+    pref[from_x][to_y + 1] -= val;
+    pref[to_x + 1][to_y + 1] += val;
+}
+void build(){
+    for(int i = 1; i <= n; ++i) for(int j = 1; j <= m; ++j){
+        pref[i][j] += pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1];
     }
-    for(int i = 0; i < q; ++i){
-        int from_x , to_x , from_y , to_y;
-        cin >> from_x >> from_y;
-        cin >> to_x >> to_y;
-        int ans = pre[to_x][to_y] - pre[from_x - 1][to_y] - pre[to_x][from_y - 1] + pre[from_x - 1][from_y - 1];
-        cout << ans << '\n';
+}
+void init(){
+    for(int i = 1; i <= n; ++i) for(int j = 1; j <= m; ++j){
+        pref[i][j] = 0;
     }
 }

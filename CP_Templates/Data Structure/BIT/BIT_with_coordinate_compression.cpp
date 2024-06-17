95672e0786ff72ff4c665a5e06bd911492d91840
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-06-17 19:25:36 +0300

    Update BIT_with_coordinate_compression.cpp

--- CP_Templates/Data Structure/BIT/BIT_with_coordinate_compression.cpp
+++ CP_Templates/Data Structure/BIT/BIT_with_coordinate_compression.cpp
@@ -1,16 +1,9 @@
-/*
-    --------------BIT-----------------
-    Use: 1 - add value to element in the array
-         2 - sum of range in the array
-    Base: 0-index
-    Time complexity : add , sum >> O(log(n))
-
-*/
 template<class T = int>
 struct BIT{
-    vector<T> tree;
+    vector<T> tree , v;
     int n;
-    void init(vector<T> &v){
+    void init(vector<T> vv){
+        this->v = vv;
         sort(all(v));
         v.erase(unique(v.begin() , v.end()) , v.end());
         this->n = v.size();
@@ -18,18 +11,16 @@ struct BIT{
     }
     void add(T idx , T val){
         int pos = lower_bound(v.begin(), v.end() , idx) - v.begin();
-        assert(pos < n && v[pos] == idx);
         for(pos++; pos <= n ; pos += (pos & (-pos)))
             tree[pos - 1] += val;
     }
     T sum(int pos){
-        pos = lower_bound(v.begin(), v.end() , idx) - v.begin();
-        assert(pos < n);
+        pos = lower_bound(v.begin(), v.end() , pos) - v.begin();
         T ret = 0;
         for(pos++; pos ; pos -= (pos & (-pos)))
             ret += tree[pos - 1];
         return ret;
     }
-    T qry(int l , int r){ return sum(r) - sum(l - 1); }
-    T qry(int i){ return sum(i , i); }
+    T qry(int l, int r){ return sum(r) - sum(l - 1); }
+    T get_idx(int i){ return qry(i , i); }
 };

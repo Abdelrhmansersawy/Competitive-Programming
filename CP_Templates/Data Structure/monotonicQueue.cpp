5e109eac0e96df50e13d04108841f5b8639ce8d2
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-06-21 17:53:23 +0300

    Update monotonicQueue.cpp

--- CP_Templates/Data Structure/monotonicQueue.cpp
+++ CP_Templates/Data Structure/monotonicQueue.cpp
@@ -4,30 +4,31 @@
 */
 template<class T>
 class monotonic_queue {
-queue<T> qu;
-deque<T> mx, mn;
-public:
-1void push(T v) {
-qu.push(v);
-while (mx.size() && mx.back() < v) mx.pop_back();
-mx.push_back(v);
-while (mn.size() && mn.back() > v) mn.pop_back();
-mn.push_back(v);
-}
-void pop() {
-if (mx.front() == qu.front()) mx.pop_front();
-if (mn.front() == qu.front()) mn.pop_front();
-qu.pop();
-}
-T front() const {
-return qu.front();
-}
-T max() const {
-return mx.front();
-}
-T min() const {
-return mn.front();
-}
-size_t size() const {
-return qu.size();
-}
+	queue<T> qu;
+	deque<T> mx, mn;
+	public:
+	void push(T v) {
+		qu.push(v);
+		while (mx.size() && mx.back() < v) mx.pop_back();
+		mx.push_back(v);
+		while (mn.size() && mn.back() > v) mn.pop_back();
+		mn.push_back(v);
+	}
+	void pop() {
+		if (mx.front() == qu.front()) mx.pop_front();
+		if (mn.front() == qu.front()) mn.pop_front();
+		qu.pop();
+	}
+	T front() const {
+		return qu.front();
+	}
+	T max() const {
+		return mx.front();
+	}
+	T min() const {
+		return mn.front();
+	}
+	size_t size() const {
+		return qu.size();
+	}
+};

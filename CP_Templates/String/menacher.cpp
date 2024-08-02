2b332a56454d1495a7f0908c170a583212e7d350
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-08-02 22:08:14 +0300

    Update menacher.cpp

--- CP_Templates/String/menacher.cpp
+++ CP_Templates/String/menacher.cpp
@@ -1,39 +1,25 @@
-vector<int> menacher(string &s) {
-    string arr;
-    for (int i = 0; i < s.size(); i++) {
-        arr.push_back('#');
-        arr.push_back(s[i]);
+vector<int> manacher_odd(string s) {
+    int n = s.size();
+    s = "$" + s + "^";
+    vector<int> p(n + 2);
+    int l = 1, r = 1;
+    for(int i = 1; i <= n; i++) {
+        p[i] = max(0, min(r - i, p[l + (r - i)]));
+        while(s[i - p[i]] == s[i + p[i]]) {
+            p[i]++;
+        }
+        if(i + p[i] > r) {
+            l = i - p[i], r = i + p[i];
+        }
     }
-    arr.push_back('#');
-    vector<int> dp(arr.size());
-    int left = 0;
-    int right = 0;
-    int lg_max = 0;
-    int idx = 0;
-    for (int i = 0; i < arr.size();) {
-        while (left > 0 && right < arr.size() - 1 &&
-               arr[left - 1] == arr[right + 1]) {
-            left--;
-            right++;
-        }
-        dp[i] = right - left + 1;
-        if (lg_max < dp[i]) {
-            lg_max = dp[i];
-            idx = i;
-        }
-        int new_center = right + (i % 2 == 0 ? 1 : 0);
-        for (int j = i + 1; j <= right; j++) {
-            dp[j] = min(dp[i - (j - i)], 2 * (right - j) + 1);
-            if (lg_max < dp[i]) {lg_max = dp[i]; idx = i; }
-            if (j + dp[i - (j - i)] / 2 == right) {new_center = j; break; }
-        }
-        i = new_center;
-        right = i + dp[i] / 2;
-        left = i - dp[i] / 2;
-    }
-    return dp;
+    return vector<int>(begin(p) + 1, end(p) - 1);
 }
-bool is_palindrome(int l , int r , vector<int> &dp){
-    l *= 2; r *= 2; r += 2;
-    return dp[(l + r) / 2] == (r - l + 1);
+
+vector<int> manacher(string s) {
+    string t;
+    for(auto c: s) {
+        t += string("#") + c;
+    }
+    auto res = manacher_odd(t + "#");
+    return vector<int>(begin(res) + 1, end(res) - 1);
 }

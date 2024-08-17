43a1ad1b9080c2daae2c80ee059278742bfb942a
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-08-17 23:02:46 +0300

    Update BIT2D.cpp

--- CP_Templates/Data Structure/2D Range Queries/BIT2D.cpp
+++ CP_Templates/Data Structure/2D Range Queries/BIT2D.cpp
@@ -8,20 +8,9 @@
 */
 template<class T = int>
 struct BIT2D {
+private:
     vector<vector<T>> tree;
     int n , m;
-    void init(int n , int m) {
-        this->n = n; this->m = m;
-        tree.assign(n , vector<T>(m , 0));
-    }
-    void add(int x, int y, T val) {
-        for (int i = x + 1; i <= n; i += (i & (-i))) {
-            for (int j = y + 1; j <= m; j += (j & (-j))) {
-                tree[i - 1][j - 1] += val;
-            }
-        }
-    }
-
     T sum(int x, int y) {
         T ret = 0;
         for (int i = x + 1; i; i -= (i & (-i))) {
@@ -31,9 +20,20 @@ struct BIT2D {
         }
         return ret;
     }
-
-    T qru(int sx, int sy, int ex, int ey) {
+public:
+    void init(int n , int m) {
+        this->n = n; this->m = m;
+        tree.assign(n , vector<T>(m , 0));
+    }
+    void upd(int x, int y, T val) {
+        for (int i = x + 1; i <= n; i += (i & (-i))) {
+            for (int j = y + 1; j <= m; j += (j & (-j))) {
+                tree[i - 1][j - 1] += val;
+            }
+        }
+    }
+    T qry(int sx, int sy, int ex, int ey) {
         return sum(ex, ey) - sum(ex, sy - 1) - sum(sx - 1, ey) + sum(sx - 1, sy - 1);
     }
-    T qru(int x , int y){ return sum(x , y , x , y); }
+    T qry(int x , int y){ return sum(x , y , x , y); }
 };

4bee7cb68b7002b41e225361b9d2c96614ba8f27
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-08-18 19:34:21 +0300

    Update 2d_sparsetable.cpp

--- CP_Templates/Data Structure/2D Range Queries/2d_sparsetable.cpp
+++ CP_Templates/Data Structure/2D Range Queries/2d_sparsetable.cpp
@@ -1,9 +1,20 @@
+/*
+    2D sparse table
+    - Memory allocation: O(nlog(n) x 4log(m))
+    - range [l..r] , r: inclusive 
+    - Base: 0-index
+    
+    Function description:
+        1. build(n , m , g): Build 2D sparse table of g[n][m] 
+        2. qry(lx , rx , ly , ry): find (min,max,sum,produc) of rectangle [lx...rx] x [ly...ry]
+           Time complexity: O(1)
+*/
 #define vi vector<int>
 #define vi2 vector<vi>
 #define vi3 vector<vi2>
 #define vi4 vector<vi3>
 #define rep(i , st , ed) for(int i = st; i < ed; i++)
-struct Sparsetable2D{
+struct sparse_2d{
     vi4 f;
     void init(int n , int m , vi2 &g){
         int lgN = 31 - __builtin_clz(n + 1);
@@ -32,7 +43,7 @@ struct Sparsetable2D{
         }
     }
     int comb(int a, int b){
-        return __gcd(a , b);
+        return __gcd(a , b); // custom operator
     }
     int qry(int l, int d, int r, int u) {
         int k1 = 31 - __builtin_clz(r - l + 1);

c8344837c418a39cc4e8987bf6ebabbdddef2c37
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-10-09 18:29:39 +0300

    Update xor_basis_lexicographically_largest.cpp

--- CP_Templates/Data Structure/Xor Basis/xor_basis_lexicographically_largest.cpp
+++ CP_Templates/Data Structure/Xor Basis/xor_basis_lexicographically_largest.cpp
@@ -1,13 +1,22 @@
+const int LOG = 30;
 struct Basis{
-    int basis[LOG]{};
+    int basis[LOG];
     int lt[LOG];
-    Basis(){ memset(lt , -1 , sizeof lt); }
+    Basis(){
+        memset(basis,0,sizeof basis);
+        memset(lt,-1,sizeof lt);
+    }
     void insert(int x , int ind){
         for(int i = LOG - 1; i >= 0; --i){
             if(!(x >> i & 1)) continue;
-            if(ind > lt[i]){
-                swap(basis[i] , x);
+            if(lt[i] == -1){
+                lt[i] = ind;
+                basis[i] = x;
+                return;
+            }
+            if(lt[i] < ind){ 
                 swap(lt[i] , ind);
+                swap(basis[i] , x);
             }
             x ^= basis[i];
         }
@@ -15,9 +24,10 @@ struct Basis{
     int max_xor(int ind){
         int x = 0;
         for(int i = LOG - 1; i >= 0; --i){
-            if((x >> i & 1) || lt[i] < ind) continue;
+            if((x >> i & 1) || (lt[i] < ind) ) continue;
             x ^= basis[i];
         }
         return x;
     }
-};
+    void reset(){ memset(lt , -1,sizeof lt); memset(basis , 0 , sizeof basis); }
+}

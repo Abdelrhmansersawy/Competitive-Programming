9d59a2b9e4d2fad130a5d4ca91105613ea2d89ba
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2024-12-05 13:57:37 +0200

    Update menacher.cpp

--- CP_Templates/String/Basic strings Algo/menacher.cpp
+++ CP_Templates/String/Basic strings Algo/menacher.cpp
@@ -1,25 +1,67 @@
-vector<int> manacher_odd(string s) {
-    int n = s.size();
-    s = "$" + s + "^";
-    vector<int> p(n + 2);
-    int l = 1, r = 1;
-    for(int i = 1; i <= n; i++) {
-        p[i] = max(0, min(r - i, p[l + (r - i)]));
-        while(s[i - p[i]] == s[i + p[i]]) {
+// Helper function to transform string for Manacher's algorithm
+string transform(string &s) {
+    string t;
+    for(auto &val : s) {
+        t += '#';
+        t += val;
+    }
+    t += '#';
+    return t;
+}
+
+// Main Manacher's algorithm implementation
+vector<int> build(string &s) {
+    string t = transform(s);
+    int n = t.size();
+    vector<int> p(n, 0);
+    int l = 0, r = 0;
+    
+    for(int i = 0; i < n; i++) {
+        p[i] = (i < r) ? min(r - i, p[l + r - i]) : 1;
+        while(i - p[i] >= 0 && i + p[i] < n && t[i + p[i]] == t[i - p[i]]) {
             p[i]++;
         }
         if(i + p[i] > r) {
-            l = i - p[i], r = i + p[i];
+            r = i + p[i];
+            l = i - p[i];
         }
     }
-    return vector<int>(begin(p) + 1, end(p) - 1);
+    return p;
 }
 
-vector<int> manacher(string s) {
-    string t;
-    for(auto c: s) {
-        t += string("#") + c;
-    }
-    auto res = manacher_odd(t + "#");
-    return vector<int>(begin(res) + 1, end(res) - 1);
+// Check if substring [l,r] is palindrome using p array
+bool IsPalindrome(int l, int r, vector<int>& p) {
+    int center = (l + r) / 2;
+    bool odd = (l % 2 == r % 2);
+    int newCenter = 2 * center + !odd + 1;
+    return (r - l + 1) <= (p[newCenter] - 1);
+}
+
+// Returns {start_index, end_index} of longest palindrome substring using p array
+pair<int,int> LongestPalindromeSubStr(vector<int>& p) {
+   int maxLength = 0;
+   int start = 0, end = 0;
+   int n = p.size() / 2;  // Original string length
+   
+   for(int i = 0; i < n; i++) {
+       // Even length palindromes
+       int newCenter = 2 * i + 2;
+       int len = p[newCenter] - 1;
+       if(len > maxLength) {
+           maxLength = len;
+           start = i - len/2 + 1;
+           end = i + len/2;
+       }
+       
+       // Odd length palindromes
+       newCenter = 2 * i + 1;
+       len = p[newCenter] - 1;
+       if(len > maxLength) {
+           maxLength = len;
+           start = i - len/2;
+           end = i + len/2;
+       }
+   }
+   
+   return {start, end};
 }

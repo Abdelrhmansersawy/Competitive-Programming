78892069ceeaa1f67e2eb631e7e2cf34a2796e81
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2025-05-05 01:31:44 +0300

    Update lazy_segtree_with_rollback.cpp

--- CP_Templates/Data Structure/Segment tree/lazy_segtree_with_rollback.cpp
+++ CP_Templates/Data Structure/Segment tree/lazy_segtree_with_rollback.cpp
@@ -1,105 +1,119 @@
-struct segtree {
-    struct node {
-        int val = 0;
-
-        node() {};
-
-        node(int val) {
-            this->val = val;
+struct segtree_with_rollback {
+private:
+    struct Node {
+        ll sum;
+        
+        Node() : sum(0) {}
+        Node(ll val) : sum(val) {}
+        
+        Node operator + (const Node &other) const {
+            Node res;
+            res.sum = sum + other.sum;
+            return res;
         }
     };
 
-    int n, N, time = 0;
-    vector<node> st;
-    vector<int> lazy, upd;
-    vector<array<int, 4>> stk;
+    struct seg_save {
+        int time;
+        int Node_idx;
+        Node node_val;
+        ll lazy_val;
+    };
 
-    int l(int p) { return p << 1; }
-
-    int r(int p) { return p << 1 | 1; }
-
-    node unit = node(1e9);
-
-    segtree() {}
-
-    segtree(int sz) : n(sz), N(1 << bit_width(unsigned(sz))),
-                      st(2 * N), lazy(vector<int>(2 * N)) {}
-
-    node conq(node a, node b) {
-        return node(min(a.val, b.val));
+    int n, time = 0;
+    vector<Node> tree;
+    vector<ll> lazy;
+    vector<int> upd_times;
+    vector<seg_save> history;
+    
+    void add_history(int Node_idx) {
+        history.push_back({time, Node_idx, tree[Node_idx], lazy[Node_idx]});
     }
-
-    void build(ll p, ll L, ll R, vector<ll> &A) {
-        if (L == R) {
-            st[p] = A[L];
-        } else {
-            ll m = (L + R) / 2;
-            build(l(p), L, m, A);
-            build(r(p), m + 1, R, A);
-            st[p] = conq(st[l(p)], st[r(p)]);
+    
+    void push_down(int x, ll lazy_val) {
+        if (lazy_val == 0) return;
+        add_history(x);  
+        tree[x].sum += lazy_val * (tree[x].sum == 0 ? 1 : tree[x].sum);   
+        lazy[x] += lazy_val;
+    }
+    
+    void propagate(int x, int l, int r) {
+        if (r - l == 1) return;
+        if (lazy[x] == 0) return;
+        
+        int mid = (l + r) / 2;
+        push_down(2 * x + 1, lazy[x]);
+        push_down(2 * x + 2, lazy[x]);
+        
+        lazy[x] = 0;
+    }
+    
+    void upd(int lx, int rx, ll val, int x = 0, int l = 0, int r = -1) {
+        if (r == -1) r = n;
+        propagate(x, l, r);
+        if (l >= rx || r <= lx) return;
+        if (l >= lx && r <= rx) {
+            add_history(x);
+            tree[x].sum += val * (r - l);
+            lazy[x] += val;
+            return;
         }
+
+        int mid = (l + r) / 2;
+        upd(lx, rx, val, 2 * x + 1, l, mid);
+        upd(lx, rx, val, 2 * x + 2, mid, r);
+        
+        add_history(x);
+        tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
     }
 
-    void add_upd(int node) {
-        stk.push_back({time, node, st[node].val, lazy[node]});
+public:
+    void init(int sz){
+        this->n = sz;
+        tree.resize(4 * n);
+        lazy.resize(4 * n, 0);
     }
 
-    void propagate(int p, int L, int R) {
-        if (lazy[p] != 0) {
-            add_upd(p);
-            st[p].val = max(st[p].val, lazy[p]);
-            if (L != R) {
-                add_upd(l(p)), add_upd(r(p));
-                lazy[l(p)] = max(lazy[l(p)], lazy[p]);
-                lazy[r(p)] = max(lazy[r(p)], lazy[p]);
-            }
-            lazy[p] = 0;
+    void build(vector<ll> &a, int x = 0, int l = 0, int r = -1) {
+        if (r == -1) r = n;
+        
+        if (r - l == 1) {
+            tree[x] = Node(a[l]);
+            return;
         }
+        
+        int mid = (l + r) / 2;
+        build(a, 2 * x + 1, l, mid);
+        build(a, 2 * x + 2, mid, r);
+        tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
     }
 
-    node query(int p, int L, int R, int i, int j) {
-        propagate(p, L, R);
-        if (j < L || i > R) return unit;
-        if ((L >= i) && (R <= j)) return st[p];
-        int m = (L + R) / 2;
-        return conq(query(l(p), L, m, i, j), query(r(p), m + 1, R, i, j));
-    }
-
-    void update(int p, int L, int R, int i, int j, int val) {
-        propagate(p, L, R);
-        if (j < L || i > R) return;
-        if (L >= i && R <= j) {
-            add_upd(p);
-            lazy[p] = val;
-            propagate(p, L, R);
-        } else {
-            int m = (L + R) / 2;
-            update(l(p), L, m, i, j, val);
-            update(r(p), m + 1, R, i, j, val);
-            add_upd(p);
-            st[p] = conq(st[l(p)], st[r(p)]);
-        }
-    }
-
-    void update(int i, int j, ll val) {
-        upd.push_back(time);
-        update(1, 0, n - 1, i, j, val);
+    void upd_with_rollback(int lx, int rx, ll val) {
+        upd_times.push_back(time);
+        upd(lx, rx, val);
         time++;
     }
-
-    node query(int i, int j) {
-        return query(1, 0, n - 1, i, j);
+    
+    Node qry(int lx, int rx, int x = 0, int l = 0, int r = -1) {
+        if (r == -1) r = n;
+        propagate(x, l, r);
+        if (l >= rx || r <= lx) return Node();
+        if (l >= lx && r <= rx) return tree[x];
+        int mid = (l + r) / 2;
+        return qry(lx, rx, 2 * x + 1, l, mid) + qry(lx, rx, 2 * x + 2, mid, r);
     }
 
-    void rollback() {
-        int last = upd.back(); upd.pop_back();
-        while (stk.size()) {
-            auto [t, node, val, lz] = stk.back();
-            if (t < last) break;
-            st[node].val = val, lazy[node] = lz;
-            stk.pop_back();
+        void rollback() {
+        if (upd_times.empty()) return;
+        
+        int last_time = upd_times.back();
+        upd_times.pop_back();
+        
+        while (!history.empty() && history.back().time == last_time) {
+            auto &save = history.back();
+            tree[save.Node_idx] = save.node_val;
+            lazy[save.Node_idx] = save.lazy_val;
+            history.pop_back();
         }
     }
-
-    void build(vector<ll> &e) { build(1, 0, n - 1, e); }
 };

695849614f8ef9e9078ff92ebc9b7cb7578e431b
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2025-06-06 21:56:59 +0300

    Update shortest_path_negative_weight.cpp

--- CP_Templates/Graph/bellman ford/shortest_path_negative_weight.cpp
+++ CP_Templates/Graph/bellman ford/shortest_path_negative_weight.cpp
@@ -15,6 +15,8 @@ Approach:
 */
 #include <bits/stdc++.h>
 using namespace std;
+
+const int inf = 2e9;
 int main(){
     ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(0);
 
@@ -23,10 +25,10 @@ int main(){
         if(!n) break;
         vector<array<int,3>> edges(m); // {u,v,w}
         for(auto &[u,v,w] : edges) cin >> u >> v >> w;
-        vector<int> dis(n , 2e9);
+        vector<int> dis(n , inf);
         dis[s] = 0; // source
         for(int i = 0; i < n; ++i){
-            for(auto &[u,v,w] : edges) if(dis[u] != 2e9){ // can reach node u from source s
+            for(auto &[u,v,w] : edges) if(dis[u] != inf){ // can reach node u from source s
                 if(dis[v] > dis[u] + w){
                     dis[v] = dis[u] + w;
                 }
@@ -34,17 +36,17 @@ int main(){
         }
 
         for(int i = 0; i < n; ++i){
-            for(auto &[u,v,w] : edges) if(dis[u] != 2e9){
+            for(auto &[u,v,w] : edges) if(dis[u] != inf){
                 if(dis[v] > dis[u] + w){
-                    dis[v] = -2e9; // can reach it through negative cycle
+                    dis[v] = -inf; // can reach it through negative cycle
                 }
             }
         }
 
         while(q--){
             int x; cin >> x;
-            if(dis[x] == 2e9) cout << "Impossible\n";
-            else if(dis[x] == -2e9) cout << "-Infinity\n";
+            if(dis[x] == inf) cout << "Impossible\n";
+            else if(dis[x] == -inf) cout << "-Infinity\n";
             else cout << dis[x] << '\n';
         }
 

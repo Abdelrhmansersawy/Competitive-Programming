ee22baf45c9a0f20b91af44e92006534083bce17
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2025-07-14 23:01:47 +0300

    Create tree_hashing.cpp

---
+++ CP_Templates/Trees/tree_hashing.cpp
@@ -0,0 +1,26 @@
+// Tree Hashing (Subtree Hashing)
+// Time Complexity: O(n log n)
+// 
+// This function computes a hash for each subtree in a rooted tree.
+// It can be used to test subtree isomorphism. Optionally, sort children
+// before hashing to ignore child order (unordered rooted tree).
+
+map<vector<int>, int> mp;
+
+void tree_hash(int u, int p, vector<int> &hash, vector<int> adj[]) {
+    vector<int> child;
+    for (auto &v : adj[u]) {
+        if (v != p) {
+            tree_hash(v, u, hash, adj);
+            child.emplace_back(hash[v]);
+        }
+    }
+
+    // Uncomment the following line if the order of children doesn't matter
+    // sort(child.begin(), child.end());
+
+    if (!mp.count(child)) {
+        mp[child] = mp.size() + 1;
+    }
+    hash[u] = mp[child];
+}

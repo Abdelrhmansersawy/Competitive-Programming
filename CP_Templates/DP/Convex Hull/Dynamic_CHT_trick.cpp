eeee44b6ff1608119a8ebd5e991b2b5b46fb8a52
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2025-07-17 00:13:52 +0300

    Update Dynamic_CHT_trick.cpp

--- CP_Templates/DP/Convex Hull/Dynamic_CHT_trick.cpp
+++ CP_Templates/DP/Convex Hull/Dynamic_CHT_trick.cpp
@@ -1,83 +1,36 @@
-#include <iostream>
-#include <set>
-#include <climits>
-
-using ll = long long;
-using namespace std;
-
 /*
  * LineContainer (Dynamic Convex Hull Trick)
  * -----------------------------------------
  * Adds lines y = kx + m and queries for the maximum (or minimum) value
  * at a given x in O(log n) time. Slopes can be added in any order.
  */
-
 struct Line {
-	// `mutable` allows p to be changed inside the set.
-	mutable ll k, m, p;
-
-	bool operator<(const Line& other) const { return k < other.k; }
-	bool operator<(ll x) const { return p < x; }
+    mutable ll k, m, p;
+    bool operator<(const Line& o) const { return k < o.k; }
+    bool operator<(ll x) const { return p < x; }
 };
-
 struct LineContainer : multiset<Line, less<>> {
-	bool is_min = false;
-	LineContainer(bool is_min_mode = false) : is_min(is_min_mode) {}
-
-	static const ll INF = LLONG_MAX;
-
-	// Integer division that floors result correctly for negative numbers.
-	ll div(ll a, ll b) {
-		return a / b - ((a > 0) != (b > 0) && a % b);
-	}
-
-	bool isect(iterator x, iterator y) {
-		if (y == end()) {
-			x->p = INF;
-			return false;
-		}
-		if (x->k == y->k) {
-			x->p = (x->m > y->m) ? INF : -INF;
-		} else {
-			x->p = div(y->m - x->m, x->k - y->k);
-		}
-		return x->p >= y->p;
-	}
-
-	void add(ll k, ll m) {
-		if (is_min) {
-			k = -k;
-			m = -m;
-		}
-		auto z = insert({k, m, 0});
-		auto y = z++;
-
-		// Remove lines made redundant by the new line.
-		while (isect(y, z)) {
-			z = erase(z);
-		}
-		if (y != begin()) {
-			auto x = prev(y);
-			if (isect(x, y)) {
-				isect(x, erase(y));
-			}
-		}
-		auto x = begin();
-		if (y != x) {
-			x = prev(y);
-			while (y != begin() && isect(x, y)) {
-				y = erase(y);
-				x = prev(y);
-			}
-		}
-	}
-
-	ll query(ll x) {
-		if (empty()) {
-			return is_min ? LLONG_MAX : LLONG_MIN;
-		}
-		auto l = *lower_bound(x);
-		ll res = l.k * x + l.m;
-		return is_min ? -res : res;
-	}
+    static const ll inf = LLONG_MAX;
+    bool isMn = false;
+    ll div(ll a, ll b) {
+        return a / b - ((a ^ b) < 0 && a % b); }
+    bool isect(iterator x, iterator y) {
+        if (y == end()) return x->p = inf, 0;
+        if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+        else x->p = div(y->m - x->m, x->k - y->k);
+        return x->p >= y->p;
+    }
+    void add(ll k, ll m) {
+        if (isMn) k = -k , m = -m;
+        auto z = insert({k, m, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
+            isect(x, erase(y));
+    }
+    ll query(ll x) {
+	if (empty())  return isMn ? LLONG_MAX : LLONG_MIN;
+        auto l = *lower_bound(x);
+        return (isMn ? -1 : 1) * (l.k * x + l.m);
+    }
 };

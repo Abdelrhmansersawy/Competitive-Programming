688176ace311469641f40d8f3ab8b494310af9b1
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2025-07-28 20:26:01 +0300

    Update and rename aho_coasick_helper_function.cpp to AhoCorsick_helper_function.cpp

--- CP_Templates/String/Suffix_Automation/Aho corasick/AhoCorsick_helper_function.cpp
+++ CP_Templates/String/Suffix_Automation/Aho corasick/AhoCorsick_helper_function.cpp
@@ -46,3 +46,22 @@ ll count(vector<string> &pat, string &word) {
     }
     return dp[word.size()];
 }
+
+// Propagates values up the suffix links in decreasing depth order
+// Useful for transferring match info (e.g., pattern IDs, frequencies, counts, etc.)
+// Nodes are processed deepest to shallowest to ensure correctness
+// Time: O(total number of nodes)
+void propagate() {
+    int N = v.size() - 1; // exclude dummy node
+    int ind[N];
+    iota(ind, ind + N, 0); // fill with 0..N-1
+    sort(ind, ind + N, [&](int x, int y) {
+        return v[x].lvl > v[y].lvl; // sort by depth descending
+    });
+    for (int i = 0; i < N; ++i) {
+        int x = ind[i];
+        int link = v[x].suflink;
+        // TODO: propagate information from node x to its suffix link
+        // Example: v[link].value += v[x].value;
+    }
+}

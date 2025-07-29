5b5b16c49eff0ff6bed6a8ba93210fba0763a39e
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2025-07-29 12:41:06 +0300

    Update kth_distinct_substring.cpp

--- CP_Templates/String/suffixArray/problems/kth_distinct_substring.cpp
+++ CP_Templates/String/suffixArray/problems/kth_distinct_substring.cpp
@@ -1,20 +1,50 @@
 /*
-Problem link: https://cses.fi/problemset/task/2108/
-You are given a string of length n.
-If all of its distinct substrings are ordered lexicographically, what is the kth smallest of them?
-*/
-int main(){
-    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(0);
-    string s; cin >> s;
-    int n = s.size();
-    auto p = buildSuffixArray(s);
-    auto lcp = Kasai(s,p);
-    ll k; cin >> k;
-    if(k <= n - p[0]){ cout << s.substr(p[0], k); return 0; }
-    k -= n - p[0];
-    for(int i = 0; i < lcp.size(); ++i){
-        int len = n - p[i+1] - lcp[i];
-        if(k <= len){ cout << s.substr(p[i+1],k+lcp[i]); return 0; }
-        k -= len;
+ * ============= K-th Lexicographical Distinct Substring (CSES 2108) =============
+ *
+ * Problem:
+ * - Given a string of length n.
+ * - Consider all **distinct** substrings sorted lexicographically.
+ * - Find the k-th one.
+ *
+ * Tools Used:
+ * - Suffix Array
+ * - LCP Array (Kasai's Algorithm)
+ *
+ * Time Complexity:
+ * - O(n log n) for suffix + LCP
+ * - O(n) to iterate and count distinct substrings
+ *
+ * Link:
+ * - https://cses.fi/problemset/task/2108/
+ *
+ * ================================================================================
+ */
+
+string s; cin >> s;
+int n = s.size();
+
+auto p = buildSuffixArray(s);
+auto lcp = Kasai(s, p);
+
+ll k; cin >> k;
+
+/*
+ * Each suffix contributes (n - p[i]) - lcp[i - 1] new distinct substrings.
+ * Iterate through suffixes and skip the total contribution until reaching k.
+ */
+
+if (k <= n - p[0]) {
+    cout << s.substr(p[0], k);
+    return 0;
+}
+
+k -= n - p[0];
+
+for (int i = 0; i < lcp.size(); ++i) {
+    int len = n - p[i + 1] - lcp[i];
+    if (k <= len) {
+        cout << s.substr(p[i + 1], k + lcp[i]);
+        return 0;
     }
+    k -= len;
 }

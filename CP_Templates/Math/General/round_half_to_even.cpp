a7ae4fc8f0f2caf106929741aca8d6e8b1c5e385
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2025-08-21 22:55:01 +0300

    Update round_half_to_even.cpp

--- CP_Templates/Math/General/round_half_to_even.cpp
+++ CP_Templates/Math/General/round_half_to_even.cpp
@@ -1,6 +1,6 @@
 // Rounds x to `decimals` places using round-half-to-even
-double round_to(double x, int decimals) {
-    double scale = pow(10.0, decimals);
+ld round_to(ld x, int decimals) {
+    ld scale = powl(10.0L, decimals);
     // rint uses current rounding mode (default = FE_TONEAREST = half-to-even)
-    return rint(x * scale) / scale;
+    return rintl(x * scale) / scale;
 }
